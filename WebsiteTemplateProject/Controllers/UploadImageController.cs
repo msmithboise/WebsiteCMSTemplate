4b5bfbe39ecbaa9531b6de02f67a99c6f20d2693
Michael <msmithboise@gmail.com>
2020-09-05 22:37:55 -0600

    added download logic to service class

--- WebsiteTemplateProject/Controllers/UploadImageController.cs
+++ WebsiteTemplateProject/Controllers/UploadImageController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
 using WebsiteTemplateProject.Models;
@@ -39,10 +40,48 @@ namespace WebsiteTemplateProject.Controllers
                 db.SaveChanges();
             }
             return Request.CreateResponse(HttpStatusCode.Created);
-
-
-
-
         }
+
+        //Download Image file
+
+        [HttpGet]
+        [Route("api/GetImage")]
+
+        public HttpResponseMessage GetImage(string image)
+        {
+            //Create HTTP Response.
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+
+            //Set the File Path.
+            string filePath = System.Web.HttpContext.Current.Server.MapPath("~/ Images /") + image + ".PNG";
+            //Check whether file exists.
+            if (!File.Exists(filePath))
+            {
+                //throw 404 (Not Found) eception if File not found.
+                response.StatusCode = HttpStatusCode.NotFound;
+                response.ReasonPhrase = string.Format("File not found: {0} .", image);
+                throw new HttpResponseException(response);
+            }
+
+            //Read the File in a Byte Array.
+            byte[] bytes = File.ReadAllBytes(filePath);
+
+            //Set the Response Content.
+            response.Content = new ByteArrayContent(bytes);
+
+            //Set the reponse content length.
+
+            response.Content.Headers.ContentLength = bytes.LongLength;
+
+            //Set the Content Disposition Header Value and FileName.
+
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentDisposition.FileName = image + ".PNG";
+            //Set the file Content Type.
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue(MimeMapping.GetMimeMapping(image + ".PNG"));
+            return response;
+            
+        }
+
     }
 }
