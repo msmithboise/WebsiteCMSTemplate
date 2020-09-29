16b104ddcd87bd4f15d867f5817051164f8f526e
Michael <msmithboise@gmail.com>
2020-09-29 16:47:27 -0600

    creating default template

---
+++ WebsiteTemplateProject/Controllers/DefaultTemplateController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebsiteTemplateProject;
+using WebsiteTemplateProject.Models;
+using WebsiteTemplateProject.Service;
+
+namespace WebsiteTemplateProject.Controllers
+{
+    public class DefaultTemplateController : ApiController
+    {
+        private NewWebContentEntities2 db = new NewWebContentEntities2();
+
+        // GET: api/DefaultTemplate
+        public IQueryable<WebContent> GetWebContents()
+        {
+            return db.WebContents;
+        }
+
+        // GET: api/DefaultTemplate/5
+        [ResponseType(typeof(WebContent))]
+        public IHttpActionResult GetWebContent(int id)
+        {
+            WebContent webContent = db.WebContents.Find(id);
+            if (webContent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(webContent);
+        }
+
+        // PUT: api/DefaultTemplate/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutWebContent(int id, WebContent webContent)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != webContent.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(webContent).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WebContentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/DefaultTemplate
+        [ResponseType(typeof(WebContent))]
+        public IHttpActionResult PostWebContent(WebContent webContent)
+        {
+            DefaultTemplateService defaultTemplateService = new DefaultTemplateService();
+
+            defaultTemplateService.CreateHeroImage(webContent,db);
+            defaultTemplateService.CreateTextHeader(webContent, db);
+
+            return CreatedAtRoute("DefaultApi", new { id = webContent.Id }, webContent);
+        }
+
+        // DELETE: api/DefaultTemplate/5
+        [ResponseType(typeof(WebContent))]
+        public IHttpActionResult DeleteWebContent(int id)
+        {
+            WebContent webContent = db.WebContents.Find(id);
+            if (webContent == null)
+            {
+                return NotFound();
+            }
+
+            db.WebContents.Remove(webContent);
+            db.SaveChanges();
+
+            return Ok(webContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool WebContentExists(int id)
+        {
+            return db.WebContents.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ WebsiteTemplateProject/Service/DefaultTemplateService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace WebsiteTemplateProject.Service
+{
+
+    public interface IDefaultTemplateService
+    {
+        WebContent UpsertDefaultTemplate(WebContent webContent);
+    }
+
+
+    public class DefaultTemplateService
+    {
+        public readonly NewWebContentEntities2 _context;
+
+        public DefaultTemplateService(NewWebContentEntities2 context)
+        {
+            _context = context;
+        }
+
+        public DefaultTemplateService()
+        {
+
+        }
+
+        public WebContent CreateHeroImage(WebContent webContent, NewWebContentEntities2 db)
+        {
+
+
+            webContent.ImageUrl = "https://images.unsplash.com/photo-1600867317504-3ea8cff32509?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1364&q=80";
+            webContent.PageId = 1;
+
+
+            db.WebContents.Add(webContent);
+
+            db.SaveChanges();
+            return webContent;
+        }
+
+
+        public WebContent CreateTextHeader(WebContent wc, NewWebContentEntities2 db)
+        {
+            wc.ImageUrl = "";
+
+            wc.PageId = 1;
+            wc.TextBody = "www.test.com";
+            wc.color = "blue";
+
+
+            db.WebContents.Add(wc);
+
+            db.SaveChanges();
+            return wc;
+        }
+    }
+}
+
+
+
+
+
+        
+
+      
+    
